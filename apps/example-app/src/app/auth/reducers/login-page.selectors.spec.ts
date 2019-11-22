0a37220dbccad28476a9d45288f36d1b8b6e7cab
Isaac Mann <isaacplmann+git@gmail.com>
2019-11-22 10:18:31 -0500

    test selectors

--- apps/example-app/src/app/auth/reducers/login-page.selectors.spec.ts
+++ apps/example-app/src/app/auth/reducers/login-page.selectors.spec.ts
@@ -1,5 +1,62 @@
+import {
+  authFeatureKey,
+  AuthState,
+  selectAuthState,
+  State,
+  selectUser,
+  selectLoggedIn
+} from './';
+
+function createAuthState(): State {
+  return <any>{
+    [authFeatureKey]: {
+      loginPage: {
+        pending: false,
+        error: undefined
+      },
+      status: {
+        user: {
+          name: 'someone'
+        }
+      }
+    }
+  };
+}
+
 describe('LoginPageSelectors', () => {
-  it('should have a test', () => {
-    // TODO
+  it('should test selectors', () => {
+    const state = createAuthState();
+    expect(selectAuthState(state)).toBe(state[authFeatureKey]);
+    expect(selectUser(state)).toBe(state[authFeatureKey].status.user);
+    expect(selectLoggedIn(state)).toBe(true);
+  });
+
+  const testCases = [
+    {
+      name: 'selectAuthState',
+      selector: selectAuthState,
+      state: createAuthState()
+    },
+    {
+      name: 'selectUser',
+      selector: selectUser,
+      state: createAuthState()
+    },
+    {
+      name: 'selectLoggedIn',
+      selector: selectLoggedIn,
+      state: createAuthState()
+    }
+  ];
+  testCases.forEach(({ name, state, selector }) => {
+    it(`should test ${name} with input ${JSON.stringify(state)}`, () => {
+      expect(selector(state)).toMatchSnapshot();
+    });
+  });
+
+  // using projector
+  it('should test just the selectLoggedIn logic', () => {
+    expect(selectLoggedIn.projector({ name: 'something' })).toBe(true);
+    expect(selectLoggedIn.projector(undefined)).toBe(false);
   });
 });
